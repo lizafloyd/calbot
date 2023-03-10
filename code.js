5ec146925f2af51591d0c59add34eab25f5e2d98
Liza Floyd <lizafloyd@Lizas-MacBook-Air.local>
2023-03-10 13:24:04 -0500

    adds readme

--- code.js
+++ code.js
@@ -4,7 +4,6 @@ const hello = () => {
 
 const calculateFare = (fareArg, moniesArg) => {
   try {
-    console.log("testing", moniesArg);
     const fare = parseInt(fareArg);
     const coins = moniesArg.split(",").map(Number);
     coins.sort((a, b) => b - a);
@@ -13,7 +12,6 @@ const calculateFare = (fareArg, moniesArg) => {
     let coinsInUse = [];
     let fareSoFar = 0;
     const returnValue = tryFares(fare, usefulCoins);
-    console.log("OMG", returnValue)
     const returnMessage =
       returnValue && returnValue.length
         ? `Use ${returnValue}`
@@ -21,7 +19,6 @@ const calculateFare = (fareArg, moniesArg) => {
     return returnMessage;
 
     function tryFares(initialFare, initialCoins) {
-      console.log("beginning loop");
       const wallet = initialCoins;
       // not enough inputs
       if (initialFare === 0 || initialCoins.length === 0) {
@@ -43,65 +40,48 @@ const calculateFare = (fareArg, moniesArg) => {
       coinsInUse.push(initialCoins[0]);
       fareSoFar = fareSoFar + initialCoins[0];
       wallet.splice(0, 1);
-      console.log("FARE FARESOFAR COINSINUSE", fare, fareSoFar, coinsInUse);
-      // OR we overshot, so we need to try again without the first value
-      // console.log("WHY", initialFare, fare);
-      // if (fareSoFar > fare) {
-      //   coinsInUse = [];
-      //   const newAttemptCoins = coins
-      //   newAttemptCoins.shift()
-      //   console.log("omg we overshot", fare, newAttemptCoins, coins);
-      //   tryFares(fare, newAttemptCoins);
-      // }
-      // now we have the correct change
+      // if we now have the correct change
       if (fare - fareSoFar === 0) {
-        console.log("did it!", fareSoFar, fare, wallet);
         return coinsInUse;
       }
-      console.log("beginning the ifs", fareSoFar, wallet, fare, wallet.length);
       // adding another coin is too much and there are NO more coins to try
       // throw away #1 and try again
       if (fareSoFar + wallet[0] > fare && wallet.length < 1) {
         coinsInUse.splice(0, 1);
-        console.log(
-          "should throw away #1",
-          fareSoFar,
-          initialCoins,
-          coinsInUse
-        );
         return tryFares(fare - fareSoFar, initialCoins);
       }
       // adding another coin is not enough and there are more coins to try
       // add #1 & #2 to fareSoFar and try again
       if (fareSoFar + wallet[0] < fare && wallet.length > 1) {
-        console.log("add both", fareSoFar, initialCoins, coinsInUse);
         return tryFares(fare - fareSoFar, wallet);
       }
       // adding another coin is not enough and there are NO more coins to try
       // drop #1 and try again
       if (fareSoFar + wallet[0] < fare && wallet.length <= 1) {
         coinsInUse.splice(0, 1);
-        console.log("drop #1", fareSoFar, initialCoins, coinsInUse);
         return tryFares(fare - fareSoFar, wallet);
       }
       // adding another coin is too much and there are more coins to try
       // throw away #2 and try again
       if (fareSoFar + wallet[0] > fare && wallet.length >= 1) {
-        console.log("SUP", wallet[0], fare-fareSoFar)
-          console.log("should throw away #2", fare - fareSoFar, wallet);
-          wallet.splice(0, 1);
-          return tryFares(fare - fareSoFar, initialCoins);
+        wallet.splice(0, 1);
+        return tryFares(fare - fareSoFar, initialCoins);
       }
       // adding another coin is the correct change
       if (fareSoFar + wallet[0] === fare) {
         coinsInUse.push(initialCoins[0]);
-        console.log("AM I HERE", coinsInUse);
         return coinsInUse;
       }
       return coinsInUse;
+    }
+  } catch (error) {
+    `Sorry, you've hit the following error: ${error.message}`;
+  }
+};
 
-      // console.log("top of tryFares", initialFare, initialCoins)
+module.exports = { hello, calculateFare };
 
+// scratchpad
       // if (initialFare >= initialCoins[0]){
       //     let fareLeft = initialFare - initialCoins[0]
       //     let coinsLeft = [...initialCoins]
@@ -136,10 +116,3 @@ const calculateFare = (fareArg, moniesArg) => {
       //   tryFares(fare, initialCoins)
       // }
       // return coinsInUse
-    }
-  } catch (error) {
-    `Sorry, you've hit the following error: ${error.message}`;
-  }
-};
-
-module.exports = { hello, calculateFare };
